924df1e17904596e5a906bd26f288bcf1a6257c5
Thrimbda <siyuan.arc@gmail.com>
2017-08-10 10:45:19 +0800

    test hook

--- dialogs/uni_intro.js
+++ dialogs/uni_intro.js
@@ -1,6 +1,6 @@
 module.exports = () => {
     bot.dialog("school_intro", [
-        (session) => builder.Prompts.choice(session, "西安交通大学，你想了解些什么？", "部门介绍|如何报名|社团福利|社团活动", builder.ListStyle.button),
+        (session) => session.send("西安交通大学，你想了解些什么？"),
         (session, results) => session.send("you said: %s", results.response.entity)
     ]);
 }
